1505553e4fd4699373e5e7c6599c6609e2f67376
aosorio <aosorio9559@gmail.com>
2022-01-31 22:11:44 -0500

    Added navbar in header, welcome title and description

--- src/components/MainTitle/MainTitle.js
+++ src/components/MainTitle/MainTitle.js
@@ -1,5 +1,26 @@
 import React from "react";
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponentsStyles/GlobalComponentsStyles";
+import { LeftSection } from "./MainTitleStyles";
 
 export const MainTitle = () => {
-  return <>Welcome to my personal portfolio</>;
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+          Hey! My name is Alejandro Osorio. I'm a software developer with skills
+          in front-end, back-end and database technologies (See below). I have
+          experience in designing and developing web applications for companies
+          in the hydroelectric sector as well as educational institutions. Come see my work!
+        </SectionText>
+      </LeftSection>
+    </Section>
+  );
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,8 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
           <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
